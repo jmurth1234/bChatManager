a67ca6373ee1472bb69c116f1b7f248b4686ad61
rymate1234 <supermarioryan@gmail.com>
2011-11-29 18:48:13 +0000

    HOW MANY MORE DO I HAVE TO DO BEFORE THIS JENKINS BUILDS FROM GITHUB

--- src/main/java/net/rymate/bchatmanager/bChatFormatter.java
+++ src/main/java/net/rymate/bchatmanager/bChatFormatter.java
@@ -1,3 +1,22 @@
+/*
+ * bChatManager - Chat management plugin for Bukkit
+ * Originally - PermissionsEx chat management plugin for Bukkit
+ * Copyright (C) 2011 rymate1234
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package net.rymate.bchatmanager;
 
 import java.util.Calendar;
