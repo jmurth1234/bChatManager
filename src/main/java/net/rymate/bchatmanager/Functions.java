0bd20d22b01afe3fb53ed926580537635edaf66b
rymate1234 <rymate@rymate-Aspire-5551.(none)>
2012-01-28 09:02:02 +0000

    Y U NO COMMIT

---
+++ src/main/java/net/rymate/bchatmanager/Functions.java
@@ -0,0 +1,131 @@
+/*
+ * bChatManager - Chat management plugin for Bukkit
+ * Originally - PermissionsEx chat management plugin for Bukkit
+ * Copyright (C) 2011 rymate1234
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package net.rymate.bchatmanager;
+
+import de.bananaco.permissions.Permissions;
+import de.bananaco.permissions.info.InfoReader;
+import de.bananaco.permissions.worlds.WorldPermissionsManager;
+import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author rymate1234
+ */
+public class Functions {
+
+    private final bChatManager plugin;
+    private final String displayNameFormat;
+    public InfoReader ir = null;
+    public WorldPermissionsManager wpm;
+
+    public Functions(bChatManager plugin) {
+        ir = Permissions.getInfoReader();
+        wpm = Permissions.getWorldPermissionsManager();
+        this.plugin = plugin;
+        this.displayNameFormat = plugin.getConfig().getString("display-name-format", this.displayNameFormat);
+    }
+
+    protected String replacePlayerPlaceholders(Player player, String format) {
+        String worldName = player.getWorld().getName();
+        return format
+                .replace("%prefix", this.colorize(ir.getPrefix(player)))
+                .replace("%suffix", this.colorize(ir.getSuffix(player)))
+                .replace("%world", worldName).replace("%player", player.getName())
+                .replace("%displayname", player.getDisplayName());
+    }
+
+    protected String replaceTime(String message) {
+        Calendar calendar = Calendar.getInstance();
+
+        if (message.contains("%h")) {
+            message = message.replace("%h", String.format("%02d", calendar.get(Calendar.HOUR)));
+        }
+
+        if (message.contains("%H")) {
+            message = message.replace("%H", String.format("%02d", calendar.get(Calendar.HOUR_OF_DAY)));
+        }
+
+        if (message.contains("%g")) {
+            message = message.replace("%g", Integer.toString(calendar.get(Calendar.HOUR)));
+        }
+
+        if (message.contains("%G")) {
+            message = message.replace("%G", Integer.toString(calendar.get(Calendar.HOUR_OF_DAY)));
+        }
+
+        if (message.contains("%i")) {
+            message = message.replace("%i", String.format("%02d", calendar.get(Calendar.MINUTE)));
+        }
+
+        if (message.contains("%s")) {
+            message = message.replace("%s", String.format("%02d", calendar.get(Calendar.SECOND)));
+        }
+
+        if (message.contains("%a")) {
+            message = message.replace("%a", (calendar.get(Calendar.AM_PM) == 0) ? "am" : "pm");
+        }
+
+        if (message.contains("%A")) {
+            message = message.replace("%A", (calendar.get(Calendar.AM_PM) == 0) ? "AM" : "PM");
+        }
+
+        return message;
+    }
+
+    protected String colorize(String string) {
+        if (string == null) {
+            return "";
+        }
+        return string.replaceAll("&([a-z0-9])", "\u00A7$1");
+    }
+
+    protected void updateDisplayNames() {
+        for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+            updateDisplayName(player);
+        }
+    }
+
+    protected void updateDisplayName(Player player) {
+        String worldName = player.getWorld().getName();
+        player.setDisplayName(colorize(replacePlayerPlaceholders(player, this.displayNameFormat)));
+    }
+
+    protected List<Player> getLocalRecipients(Player sender, String message, double range) {
+        Location playerLocation = sender.getLocation();
+        List<Player> recipients = new LinkedList<Player>();
+        double squaredDistance = Math.pow(range, 2);
+        for (Player recipient : Bukkit.getServer().getOnlinePlayers()) {
+            // Recipient are not from same world
+            if (!recipient.getWorld().equals(sender.getWorld())) {
+                continue;
+            }
+            if (playerLocation.distanceSquared(recipient.getLocation()) > squaredDistance && !sender.hasPermission("bchatmanager.heareverything")) {
+                continue;
+            }
+            recipients.add(recipient);
+        }
+        return recipients;
+    }
+}
