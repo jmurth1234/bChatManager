8463a3e8db9653729f4afba3fb632a37dede324f
rymate1234 <rymate@rymate-Aspire-5551.(none)>
2012-02-02 19:33:19 +0000

    Moar variables!

--- src/main/java/net/rymate/bchatmanager/Channel.java
+++ src/main/java/net/rymate/bchatmanager/Channel.java
@@ -1,13 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.rymate.bchatmanager;
 
+import java.util.List;
+import org.bukkit.entity.Player;
+
 /**
- *
+ * Represents a chat channel. These will be managed by ChannelManager hopefully.
+ * You'll also be able to hook one directly by getting them through the ChannelManager
+ * That's if I ever get round to doing an API ;)
+ * It'll definitely be used internally :D
  * @author rymate
  */
 public class Channel {
-    
+    public String name; //kinda obvious
+    public boolean isPublic; //in case I implement a list of channels (might make a spout one eventually)
+    List<Player> usersInChannel; //duh
+    public boolean isPassworded;
+    public String password;
 }
