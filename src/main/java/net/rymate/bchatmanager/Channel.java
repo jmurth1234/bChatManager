7d9c5bb624be9ac089c7e2f0919ef5030b68c4e0
rymate1234 <rymate@rymate-Aspire-5551.(none)>
2012-02-02 19:14:00 +0000

    Ok, lets get on with this! Started work on the Channel class. Also do I sees a lappy? :P

---
+++ src/main/java/net/rymate/bchatmanager/Channel.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.rymate.bchatmanager;
+
+/**
+ *
+ * @author rymate
+ */
+public class Channel {
+    
+}
