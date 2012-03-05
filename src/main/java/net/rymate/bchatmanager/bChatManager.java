54232717b2085fb03162d5d51d4e0357dd459f37
rymate1234 <Ryan@rymate-laptop.home>
2012-03-05 21:05:43 +0000

    Meh. Lets try /me in the main plugin class :(

--- src/main/java/net/rymate/bchatmanager/bChatManager.java
+++ src/main/java/net/rymate/bchatmanager/bChatManager.java
@@ -20,12 +20,18 @@
 package net.rymate.bchatmanager;
 
 import java.io.File;
+import java.util.List;
 import java.util.logging.Logger;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Main class for bChatManager
- * 
+ *
  * @author t3hk0d3
  * @author rymate1234
  */
@@ -39,7 +45,6 @@ public class bChatManager extends JavaPlugin {
     @Override
     public void onEnable() {
         setupConfig();
-        setupCommands();
         this.getServer().getPluginManager().registerEvents(this.listener, this);
         try {
             Metrics metrics = new Metrics();
@@ -57,16 +62,58 @@ public class bChatManager extends JavaPlugin {
     }
 
     public void setupConfig() {
-        configFile = new File (this.getDataFolder() + File.separator + "config.yml");
+        configFile = new File(this.getDataFolder() + File.separator + "config.yml");
         config = new Configuration(configFile);
         config.initialize();
         this.listener = new bChatListener(configFile, this);
     }
 
-    public void setupCommands() {
-        boolean use = config.getBoolean("toggles.control-me", true);
-        if (use == true) {
-            getCommand("me").setExecutor(new MeCommand(configFile, this));
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (("me".equals(command.getName())) && (config.getBoolean("toggles.control-me", true))) {
+            String meFormat = config.getString("formats.me-format", "* %player %message");
+            Double chatRange = config.getDouble("other.chat-range", 100);
+            boolean rangedMode = config.getBoolean("toggles.ranged-mode", false);
+            Functions f = new Functions(this);
+            if (args.length < 1) {
+                sender.sendMessage(ChatColor.RED + "Ya need to type something after it :P");
+                return false;
+            }
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(ChatColor.RED + "You are not an in-game player!");
+                return true;
+            }
+            Player player = (Player) sender;
+            int i;
+            StringBuilder me = new StringBuilder();
+            for (i = 0; i < args.length; i++) {
+                me.append(args[i]);
+                me.append(" ");
+            }
+            String meMessage = me.toString();
+            String message = meFormat;
+            message = f.colorize(message);
+
+            if (sender.hasPermission("bchatmanager.chat.color")) {
+                meMessage = f.colorize(meMessage);
+            }
+
+            message = message.replace("%message", meMessage).replace("%displayname", "%1$s");
+            message = f.replacePlayerPlaceholders(player, message);
+            message = f.replaceTime(message);
+
+            if (rangedMode) {
+                List<Player> pl = f.getLocalRecipients(player, message, chatRange);
+                for (int j = 0; j < pl.size(); j++) {
+                    pl.get(j).sendMessage(message);
+                }
+                sender.sendMessage(message);
+                System.out.println(message);
+            } else {
+                getServer().broadcastMessage(message);
+            }
+            return true;
         }
+        return true;
     }
 }
