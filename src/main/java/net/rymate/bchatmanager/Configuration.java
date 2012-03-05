f4ca517a96fef26a04b39b6c42a1fffc3631bf5a
rymate1234 <Ryan@rymate-laptop.home>
2012-03-05 16:15:54 +0000

    Why don't I take all the configuration stuff and shove it into one file!

---
+++ src/main/java/net/rymate/bchatmanager/Configuration.java
@@ -0,0 +1,15 @@
+package net.rymate.bchatmanager;
+
+import java.io.File;
+
+/**
+ *
+ * @author Ryan
+ */
+public class Configuration {
+    
+    public Configuration (File f) {
+        
+    }
+    
+}
