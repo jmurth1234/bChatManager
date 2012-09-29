9c6dbd9cd5fc9107a16d98ff69aa79707cdfd738
Ryan Murthick <rymate@rymate.co.uk>
2012-09-29 15:42:44 +0100

    Removed unused import

--- src/main/java/net/rymate/bchatmanager/bChatManager.java
+++ src/main/java/net/rymate/bchatmanager/bChatManager.java
@@ -32,7 +32,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.mcstats.Metrics;
