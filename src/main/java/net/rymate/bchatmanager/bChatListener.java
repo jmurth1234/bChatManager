0caeffb059c00606edc070ab3ad2843db4311f06
rymate1234 <ryan@rymate.co.uk>
2013-06-12 19:35:26 +0100

    You can now disable special features

--- src/main/java/net/rymate/bchatmanager/bChatListener.java
+++ src/main/java/net/rymate/bchatmanager/bChatListener.java
@@ -23,6 +23,7 @@ public class bChatListener implements Listener {
     public String PERSONAL_MESSAGE_FORMAT = "[MSG] [%player -> %reciever] &f%message";
     public String OP_MESSAGE_FORMAT = "&c[OPS ONLY] %player: &f%message";
     public Boolean RANGED_MODE = false;
+    public Boolean SPECIAL_FEATURES = true;
     public double CHAT_RANGE = 100d;
     private final bChatManager plugin;
     YamlConfiguration config;
@@ -33,6 +34,8 @@ public class bChatListener implements Listener {
         this.LOCAL_MESSAGE_FORMAT = plugin.config.getString("formats.local-message-format", this.LOCAL_MESSAGE_FORMAT);
         this.PERSONAL_MESSAGE_FORMAT = plugin.config.getString("formats.personal-message-format", this.PERSONAL_MESSAGE_FORMAT);
         this.RANGED_MODE = plugin.config.getBoolean("toggles.ranged-mode", this.RANGED_MODE);
+        this.SPECIAL_FEATURES = plugin.config.getBoolean("toggles.special-features", this.SPECIAL_FEATURES);
+
         this.CHAT_RANGE = plugin.config.getDouble("other.chat-range", this.CHAT_RANGE);
     }
 
@@ -56,43 +59,43 @@ public class bChatListener implements Listener {
             event.getRecipients().addAll(plugin.getLocalRecipients(player, message, range));
             event.getRecipients().addAll(plugin.getSpies());
         }
+        if (SPECIAL_FEATURES) {
+            if (chatMessage.startsWith("@") && player.hasPermission("bchatmanager.chat.message")) {
+                chatMessage = chatMessage.substring(1);
+                String[] messageSplit = chatMessage.split(" ");
+                Player reciever = plugin.getServer().getPlayer(messageSplit[0]);
+                if (messageSplit[0] == "ops") {
+                    chatMessage = chatMessage.replaceFirst(messageSplit[0], "");
+                    chatMessage = chatMessage.replaceAll("%reciever", messageSplit[0]);
 
-        if (chatMessage.startsWith("@") && player.hasPermission("bchatmanager.chat.message")) {
-            chatMessage = chatMessage.substring(1);
-            String[] messageSplit = chatMessage.split(" ");
-            Player reciever = plugin.getServer().getPlayer(messageSplit[0]);
-            if (messageSplit[0] == "ops") {
-                chatMessage = chatMessage.replaceFirst(messageSplit[0], "");
-                chatMessage = chatMessage.replaceAll("%reciever", messageSplit[0]);
+                    List<Player> recipients = new LinkedList<Player>();
+                    event.getRecipients().clear();
+                    event.getRecipients().add(player);
 
-                List<Player> recipients = new LinkedList<Player>();
-                event.getRecipients().clear();
-                event.getRecipients().add(player);
-
-                for (Player recipient : plugin.getServer().getOnlinePlayers()) {
-                    if (recipient.isOp()) {
-                        recipients.add(recipient);
+                    for (Player recipient : plugin.getServer().getOnlinePlayers()) {
+                        if (recipient.isOp()) {
+                            recipients.add(recipient);
+                        }
                     }
-                }
 
-                event.getRecipients().addAll(recipients);
-                message = PERSONAL_MESSAGE_FORMAT;
-            } else if (reciever == null) {
-                player.sendMessage("This player isn't online or you just typed the @ symbol! Ignoring.");
-                event.setCancelled(true);
-            } else {
-                chatMessage = chatMessage.replaceFirst(messageSplit[0], "");
-                message = PERSONAL_MESSAGE_FORMAT;
-                message = message.replaceAll("%reciever", messageSplit[0]);
-                localChat = false;
-                event.getRecipients().clear();
-                event.getRecipients().add(player);
-                event.getRecipients().add(reciever);
-                event.getRecipients().addAll(plugin.getSpies());
-                message = PERSONAL_MESSAGE_FORMAT;
+                    event.getRecipients().addAll(recipients);
+                    message = PERSONAL_MESSAGE_FORMAT;
+                } else if (reciever == null) {
+                    player.sendMessage("This player isn't online or you just typed the @ symbol! Ignoring.");
+                    event.setCancelled(true);
+                } else {
+                    chatMessage = chatMessage.replaceFirst(messageSplit[0], "");
+                    message = PERSONAL_MESSAGE_FORMAT;
+                    message = message.replaceAll("%reciever", messageSplit[0]);
+                    localChat = false;
+                    event.getRecipients().clear();
+                    event.getRecipients().add(player);
+                    event.getRecipients().add(reciever);
+                    event.getRecipients().addAll(plugin.getSpies());
+                    message = PERSONAL_MESSAGE_FORMAT;
+                }
             }
         }
-
         message = plugin.replacePlayerPlaceholders(player, message);
         message = plugin.colorize(message);
 
